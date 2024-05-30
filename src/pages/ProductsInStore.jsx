a4798efb2b2ab67d6b0e4e9950368f77ba0eed89
Kariaki58 <kariakistephen809@gmail.com>
2024-05-30 05:09:38 +0100

    update image to cloudinary and fixed image preview

--- src/pages/ProductsInStore.jsx
+++ src/pages/ProductsInStore.jsx
@@ -10,6 +10,8 @@ function ProductsInStore() {
   const dispatch = useDispatch();
   const { content, status } = useSelector((state) => state.userupload);
   const [products, setProducts] = useState([]);
+  const [img, setImg] = useState(null)
+  
 
   useEffect(() => {
     dispatch(getUserUploads());
@@ -23,25 +25,71 @@ function ProductsInStore() {
     document.getElementById(`image-${index}`).click();
   };
 
-  const handleTextChange = (e, index, productId) => {
+  const uploadFile = async (file, timestamp, signature) => {
+    const folder = 'images';
+    const data = new FormData();
+    data.append('file', file);
+    data.append('timestamp', timestamp);
+    data.append('signature', signature);
+    data.append('api_key', import.meta.env.VITE_APP_CLOUDINARY_API_KEY);
+    data.append('folder', folder);
+
+    try {
+      const cloudName = import.meta.env.VITE_APP_CLOUDINARY_CLOUD_NAME;
+      const resourceType = 'image';
+      const api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+
+      const res = await axios.post(api, data);
+      const { secure_url } = res.data;
+      return secure_url;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getSignatureForUpload = async (folder) => {
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_APP_BACKEND_BASEURL}/api/gensignature`,
+        { folder },
+        { withCredentials: true }
+      );
+      return res.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signedUpload = async (img) => {
+    const { timestamp: imgTimestamp, signature: imgSignature } = await getSignatureForUpload('images');
+    const imgUrl = await uploadFile(img, imgTimestamp, imgSignature);
+    return imgUrl;
+  };
+
+  const handleTextChange = async (e, index, productId) => {
     const updatedProducts = [...products];
     updatedProducts[index] = { ...updatedProducts[index], [e.target.name]: e.target.value };
     dispatch(modifyProduct(updatedProducts))
     setProducts(updatedProducts);
-    axios.put("http://localhost:5000/api/product/edit", { product: updatedProducts[index], productId }, { withCredentials: true })
+    await axios.put("http://localhost:5000/api/product/edit", { product: updatedProducts[index], productId }, { withCredentials: true })
   };
 
-  const handleFileChange = (e, index) => {
+  const handleFileChange = async (e, index, productId) => {
     const file = e.target.files[0];
     if (file) {
+      setImg(file)
+      const update = await signedUpload(file)
       const reader = new FileReader();
+      
       reader.onload = () => {
         const updatedProducts = [...products];
-        updatedProducts[index] = { ...updatedProducts[index], imgUrl: reader.result };
+        updatedProducts[index] = { ...updatedProducts[index], imgUrl: reader.result || update};
         setProducts(updatedProducts);
         dispatch(modifyProduct(updatedProducts))
       };
       reader.readAsDataURL(file);
+      const updateProduct = {...products[index], imgUrl: update}
+      await axios.put('http://localhost:5000/api/product/edit', { product: updateProduct, productId }, { withCredentials: true })
     }
   };
 
@@ -80,7 +128,7 @@ function ProductsInStore() {
               id={`image-${index}`}
               className="hidden"
               accept="image/*"
-              onChange={(e) => handleFileChange(e, index)}
+              onChange={(e) => handleFileChange(e, index, item._id)}
             />
           </form>
         </div>
