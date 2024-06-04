4d2823efa6c107b63b5a218f8291f1731b834c69
Kariaki58 <kariakistephen809@gmail.com>
2024-06-04 16:56:19 +0100

    display area chart with backend data point

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { getOrders } from '../store/orders/orderGet';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { ThreeDots } from 'react-loader-spinner';
 import { AreaChart, Tooltip, XAxis, YAxis, Area, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 
 const Stats = () => {
   const dispatch = useDispatch()
+  const [data, setData] = useState([])
 
     useEffect(() => {
       dispatch(getOrders())
     }, [dispatch])
 
-    const { orders, error, loading } = useSelector((state) => state.order)
+    const getBackendData = async () => {
+      const response = await axios.get(`${import.meta.env.VITE_APP_BACKEND_BASEURL}/api/chart/data`, { withCredentials: true })
 
+      setData(prev => response.data)
+      console.log(data)
+    }
+
+    useEffect(() => {
+      getBackendData()
+    }, [])
+    
+    console.log(data)
+
+    const { orders, error, loading } = useSelector((state) => state.order)
+    console.log(data)
     if (loading) {
       return (
         <ThreeDots
@@ -30,50 +45,6 @@ const Stats = () => {
         />
       )
     }
-    const data = [
-      {
-        "name": "Page A",
-        "uv": 4000,
-        "pv": 2400,
-        "amt": 2400
-      },
-      {
-        "name": "Page B",
-        "uv": 3000,
-        "pv": 1398,
-        "amt": 2210
-      },
-      {
-        "name": "Page C",
-        "uv": 2000,
-        "pv": 9800,
-        "amt": 2290
-      },
-      {
-        "name": "Page D",
-        "uv": 2780,
-        "pv": 3908,
-        "amt": 2000
-      },
-      {
-        "name": "Page E",
-        "uv": 1890,
-        "pv": 4800,
-        "amt": 2181
-      },
-      {
-        "name": "Page F",
-        "uv": 2390,
-        "pv": 3800,
-        "amt": 2500
-      },
-      {
-        "name": "Page G",
-        "uv": 3490,
-        "pv": 4300,
-        "amt": 2100
-      }
-    ]
 
   return (
       <section className='h-full w-[80%] mt-5 p-5 relative'>
@@ -114,12 +85,12 @@ const Stats = () => {
                     <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
                   </linearGradient>
                 </defs>
-                <XAxis dataKey="name" />
-                <YAxis dataKey="pv"/>
+                <XAxis dataKey="minute" />
+                <YAxis dataKey="totalQuantity"/>
                 {/* <CartesianGrid strokeDasharray="3 3" /> */}
                 <Tooltip />
-                <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                <Area type="monotone" dataKey="totalOrders" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="totalQuantity" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
               </AreaChart>
             </ResponsiveContainer>
             </div>
