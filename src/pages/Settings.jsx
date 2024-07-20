5ca6da69e5ebbae42a315a12acb29928389a51e8
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-07-20 19:51:31 +0000

    feat: Boilerplate for user settings form

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -8,6 +8,7 @@ import images from '../assets';
 import { useDispatch, useSelector } from 'react-redux';
 import { userProfile } from '../store/Profile/userProfile';
 import DashBoardLeft from './DashboardLeft';
+import SettingsForm from './SettingsForm';
 
 const Settings = () => {
   return (
@@ -17,8 +18,23 @@ const Settings = () => {
       </div>
 
       {/* will work on this later*/}
-      <div className="w-4/5 bg-white flex justify-center items-center">
-        <p className="text-black text-lg">Right Side</p>
+      <div className="w-4/5 bg-[#1f2937] flex">
+
+       <div className="flex flex-col w-1/3 items-center text-white">
+          <div className="avatar">
+            <div className="ring-primary ring-offset-base-100 w-24 h-24 rounded-full ring ring-offset-2 mt-9">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className="object-cover rounded-full" />
+
+            </div>
+          </div>
+          <button className="btn btn-active bg-blue-500 mt-4 px-16 py-[10px] rounded-2xl">Edit pic</button>
+        </div>
+
+        {/* Right larger section */}
+  <div className="flex-1 p-8">
+    <SettingsForm />
+        </div>
+
       </div>
     </div>
   );
