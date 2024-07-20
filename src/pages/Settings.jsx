c5fdc4daff7eeea808fe94b8662805cfab351e69
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-07-20 20:14:38 +0000

    feat: added button to arrow in settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -9,6 +9,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { userProfile } from '../store/Profile/userProfile';
 import DashBoardLeft from './DashboardLeft';
 import SettingsForm from './SettingsForm';
+import { IoIosArrowRoundBack } from "react-icons/io";
 
 const Settings = () => {
   return (
@@ -20,17 +21,29 @@ const Settings = () => {
       {/* will work on this later*/}
       <div className="w-4/5 bg-[#1f2937] flex">
 
-       <div className="flex flex-col w-1/3 items-center text-white">
+       <div className="flex flex-col w-1/3 justify-start text-white">
+
+       <div className="flex gap-2 mt-2">
+        <button><IoIosArrowRoundBack className="w-[25px] h-[25px]"/></button>
+        <h1> Settings </h1>
+       </div>
+
+       <div className="flex justify-center">
           <div className="avatar">
             <div className="ring-primary ring-offset-base-100 w-24 h-24 rounded-full ring ring-offset-2 mt-9">
               <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className="object-cover rounded-full" />
 
             </div>
           </div>
-          <button className="btn btn-active bg-blue-500 mt-4 px-16 py-[10px] rounded-2xl">Edit pic</button>
+          </div>
+
+          <div className="flex justify-center  mt-4">
+          <button className="btn btn-active bg-blue-500 rounded-2xl text-center">Edit pic</button>
+          </div>
+
         </div>
 
-        {/* Right larger section */}
+  
   <div className="flex-1 p-8">
     <SettingsForm />
         </div>
